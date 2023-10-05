b2ec5bc78087b5069890ae4e5202abcf00655eaa
ivanakhmetev <78900410+ivanakhmetev@users.noreply.github.com>
2023-10-05 22:04:23 +0300

    Add files via upload

---
+++ ex_2.sql
@@ -0,0 +1,13 @@
+Contacts: не связана с другими
+Categories - Products: один ко многим
+Products - Order Details: один ко многим
+Suppliers - Products: один ко многим
+Orders - Order Details: один ко многим
+Customers - Orders: один ко многим
+Employees - Orders: один ко многим
+Shippers - Orders: один ко многим
+Employees - Orders: один ко многим
+Employees - EmployeeTerritories: один ко многим
+Employees - Employees: один ко многим
+Territories - EmployeeTerritories: один ко многим
+Region - Territories: один ко многим
